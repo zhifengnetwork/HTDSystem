abccfa3d5dd59f40bea334d856220b14ca0eafb4
zxz <1026853966@qq.com>
2019-03-16 19:03:03 +0800

    Withdrawal.php

---
+++ application/admin/controller/Withdrawal.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+use app\common\controller\AdminBase;
+use think\Db;
+
+/**
+ * 提现
+ */
+class Withdrawal extends AdminBase
+{
+	public function withdrawal()
+	{
+		return fetch();
+	}
+}
