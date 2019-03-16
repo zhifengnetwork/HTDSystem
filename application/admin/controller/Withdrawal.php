e6e1fb113dab0825ef835d94579a0c2b6bb1cab7
zxz <1026853966@qq.com>
2019-03-16 23:39:02 +0800

    提现审核

--- application/admin/controller/Withdrawal.php
+++ application/admin/controller/Withdrawal.php
@@ -8,8 +8,62 @@ use think\Db;
  */
 class Withdrawal extends AdminBase
 {
+	protected function _initialize()
+    {
+        parent::_initialize();
+    }
+
+	/** 
+	*  列表
+	*/
+	public function withdrawal_list()
+	{
+		$info = Db::name('user_extract')->order('id desc')->paginate(10);
+		$this->assign('info',$info);
+
+		return $this->fetch();
+	}
+
+	/** 
+	*  提现
+	*/
 	public function withdrawal()
 	{
-		return fetch();
+		// return $this->fetch();
+	}
+
+	/** 
+	*  审核
+	*/
+	public function withdrawal_audit()
+	{
+		$id = I('id/d');
+		$status = I('id/d');
+
+		$info = Db::name('user_extract')->where('id',$status)->update(['status'=>$status]);
+
+		if ($info != false) {
+			return json(array('code' => 200, 'msg' => '更新成功'));
+		} else {
+			return json(array('code' => 0, 'msg' => '更新失败'));
+		}
+	}
+
+	/** 
+	*  删除
+	*/
+	public function del()
+	{
+		$id = I('id/d');
+
+		if ($id) {
+			$bool = Db::name('user_extract')->delete($id);
+		}
+		
+		if ($bool) {
+			return json(array('code' => 200, 'msg' => '删除成功'));
+		} else {
+			return json(array('code' => 0, 'msg' => '删除失败'));
+		}
 	}
 }
