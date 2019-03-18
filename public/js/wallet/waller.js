725bc076c4ef6024a2ff79b1138c47507ed07ee9
z1976718128 <1976718128@qq.com>
2019-03-18 16:57:54 +0800

    xs

--- public/js/wallet/waller.js
+++ public/js/wallet/waller.js
@@ -24,12 +24,10 @@ $(document).ready(function(){
 
 function wallet(aaa){
 	$("#span").html(aaa);
-	console.log(aaa);
+	console.log(aaa);	
 }
-
 function sc(){
 	var animateimg = $("#f").val(); //获取上传的图片名 带//
-	
 	var imgarr=animateimg.split('\\'); //分割
 	var myimg=imgarr[imgarr.length-1]; //去掉 // 获取图片名
 	var houzui = myimg.lastIndexOf('.'); //获取 . 出现的位置
@@ -38,7 +36,10 @@ function sc(){
 	
 	var file = $('#f').get(0).files[0]; //获取上传的文件
 	var fileSize = file.size;           //获取上传的文件大小
-	var maxSize = 1048576;              //最大1MB
+	var maxSize = 1048576; //最大1MB
+//	console.log(file)
+	
+	
 	if(ext !='.PNG' && ext !='.GIF' && ext !='.JPG' && ext !='.JPEG' && ext !='.BMP'){
 			parent.layer.msg('文件类型错误,请上传图片类型');
 			return false;
@@ -53,7 +54,6 @@ function sc(){
 		// console.log(name);
 		// console.log(name);
 		// console.log(name);
-		console.log(file);
 			$.ajax({  
 					url: "http://www.szpt.com/index/wallet/uppic", 
 					type: 'POST',  
@@ -72,14 +72,73 @@ function sc(){
 							var result1 = '';
 							// $("#show").attr('value',+ ret['f'] +);
 							result += '<img src="' + '__ROAD__' + ret['f']  + '" width="100">';
-							result1 += '<input value="' + ret['f']  + '" name="user_headimg" style="display:none;">';
+							result1 += '<input uploadFile value="' + ret['f']  + '" name="user_headimg" style="display:none;" webkitRelativePath>';
 							$('#result').html(result);
 							$('#show').html(result1);
 							layer.msg('上传成功');
+							console.log(ret['isSuccess'])
 					}else{  
 							layer.msg('上传失败');
 					}  
+					
 			});  
 			return false;
 		 }  
 	}
+	
+//判断有内容则显示进度条
+	$("#f").on('change', function( e ){
+         //e.currentTarget.files 是一个数组，如果支持多个文件，则需要遍历
+         //上传图片
+        var rd = new FileReader();//创建文件读取对象
+        var files = f.files[0];//获取file组件中的文件
+        rd.readAsDataURL(files);//文件读取装换为base64类型
+        var name = e.currentTarget.files[0].name;
+        console.log(e.currentTarget.files.length)
+        //添加图片
+//      $(".tu").append(`<img src='__public__/${name}'/>`)
+        if(e.currentTarget.files.length ==1){
+        	$("#wrapper").show()
+        }
+    });
+	
+
+//进度条
+var Loader = function () {    
+  var loader = document.querySelector('.loader-container'),
+      meter = document.querySelector('.meter'),
+      k, i = 1,
+      counter = function () {
+        if (i <= 100) {   
+          meter.innerHTML = i.toString();
+          i++;
+        } else {
+          window.clearInterval(k);
+        }
+      };
+	
+	return {
+  	init: function (options) {
+      options = options || {};
+      var time = options.time ? options.time : 0,
+	        interval = time/100;
+	     console.log($(".loader-container").width())
+	     if ($(".loader-container").width() == 300) {
+        	$("#wrapper").hide()
+	     	
+	     }
+    	loader.classList.add('run');
+      k = window.setInterval(counter, interval); 
+      setTimeout(function () {        
+      	loader.classList.add('done');
+      }, time);
+    },
+  }
+}();
+
+Loader.init(
+	{
+  	time: 3000,
+  	}
+  		
+	);
