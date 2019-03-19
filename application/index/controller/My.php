74df6563adcfb5c417f1d77ce0fe5f0a4032e5d9
snowlyx <snowxtlyx@163.com>
2019-03-19 21:22:11 +0800

    tijiao5

--- application/index/controller/My.php
+++ application/index/controller/My.php
@@ -25,11 +25,6 @@ class My extends HomeBase
         return view();
        
     }
-    //修改密码
-    public function set_pass()
-    {
-        return view();
-    }
     //退出登录
    public function index()
     {
@@ -42,17 +37,17 @@ class My extends HomeBase
     }
     
     
-    public function editUser()
+    public function edituser()
     {
         return $this->fetch();
     }
 
-    public function setPass()
+    public function set_pass()
     {
         return $this->fetch();
     }
 
-    public function upIDCardNew()
+    public function up_id_card_new()
     {
         return $this->fetch();
     }
