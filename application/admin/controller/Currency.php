60479061e802a9dc35812cea41849ea426d8107b
snowlyx <snowxtlyx@163.com>
2019-03-21 20:41:10 +0800

    tijao

--- application/admin/controller/Currency.php
+++ application/admin/controller/Currency.php
@@ -94,6 +94,7 @@ class Currency extends AdminBase
         if($file1){
             //上传文件
             $res = DB::name('currency')->where('id',$id)->find();
+            dump($res);die;
             //判断文件是否存在
             if (is_file($res['wallet_qrcode'])) {
                 if ($res['wallet_qrcode']) {
