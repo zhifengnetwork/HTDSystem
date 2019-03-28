22f25d0a72033154fb3f7384fa1b359294b04909
zr <zr@qq.com>
2019-03-28 16:01:31 +0800

    修改下载链接

--- application/index/controller/Download.php
+++ application/index/controller/Download.php
@@ -5,7 +5,7 @@ class Download
     
     public function download_link()
     {
-        $url = 'https://fir.im/f7vp';
+        $url = 'https://fir.im/tk9v';
         header("refresh:1;url=$url");
     }
 }
